50b298db053256bdc48e8b9cc41445563f0a8c18
Alexis Lejeune <alexis.lejeune@gmx.fr>
2021-02-10 17:24:58 +0100

    Push teletravail

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 // import "./style.css";
 import moment from 'moment';
+import regeneratorRuntime from "regenerator-runtime"; // Must be imported to use async/await
+
+const getUserModule = () => import(/* webpackChunkName: "usersAPI" */ "./components/usersAPI");
+
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+    getUserModule().then( async ({ getUsers }) => {
+        console.log(await getUsers());
+    })
+});
