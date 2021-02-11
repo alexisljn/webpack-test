7c1808d3d43f782bc348140e4bb19877a0bc5c12
Alexis <alexis.lejeune@gmx.fr>
2021-02-11 11:10:34 +0100

    Try to dynamically load React Component

---
+++ src/components/AnotherComponent.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class AnotherComponent extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <p>Component ACTIVÉ !!!!</p>
+        );
+    }
+
+}
+
+export default AnotherComponent
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,19 +1,44 @@
 import React from "react";
-
+import regeneratorRuntime from "regenerator-runtime"; // Must be imported to use async/await
 import OneComponent from "./OneComponent";
-
+// import AnotherComponent from "./AnotherComponent";
+const getAnotherComponentModule = () => import(/* webpackChunkName: "anotherComponent" */ "./AnotherComponent");
 
 class MainComponent extends React.Component {
+
     constructor(props) {
         super(props);
         this.state = {
-            nicknames: ['toto', 'momo', 'jojo']
+            nicknames: ['toto', 'momo', 'jojo'],
+            buttonActivated: false,
         };
         console.log('MaiNComponentState', this.state);
     }
 
+    onButtonClick() {
+
+        this.setState({
+            buttonActivated: true
+        });
+    };
+
+    async loadJSX() {
+        const AnotherComponent = await getAnotherComponentModule();
+        return <AnotherComponent/>;
+    }
+
 
     render() {
+        let AnotherComponent;
+
+        if (this.state.buttonActivated) {
+            (async () => {
+                AnotherComponent  = await getAnotherComponentModule();
+                // const jsxComponent = <AnotherComponent/>;
+                console.log(AnotherComponent)
+            })()
+        }
+
         return (
             <div>
                 <p>Main Component React</p>
@@ -25,6 +50,15 @@ class MainComponent extends React.Component {
 
                     )
                 })}
+                <p>If you click on the button below me you will trigger a new component</p>
+                <button onClick={() => this.onButtonClick()}>Click</button>
+                {this.state.buttonActivated
+                    ?
+                        // 'toto'
+                        <AnotherComponent/>
+                    :
+                        null
+                }
             </div>
         )
     }
